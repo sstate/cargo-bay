15e8344fd767d1caf63c80db86ebd2d8588ed687
Richard Torruellas <rtorruellas@gmail.com>
2015-02-17 09:57:05 -0700

    init commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# cargo-bay
+
+![cargo bay](http://i.imgur.com/sU7JTWh.jpg)
+
+A base store for flux applications
+
+#### Run examples
+
+```
+npm install
+
+npm run examples
+```
+
+#### Run tests
+
+```
+npm install
+
+npm test
+```
---
+++ src/index.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var LCARS = require('lcars');
+var EventEmitter = require('events').EventEmitter;
+var merge = require('amp-merge');
+var CHANGE_EVENT = 'change';
+var _current_cid = 0;
+
+var CargoBay = merge(EventEmitter.prototype, {
+
+  emitChange: function() {
+    this.emit(CHANGE_EVENT);
+  },
+
+  addChangeListener: function(callback) {
+    this.on(CHANGE_EVENT, callback);
+  },
+
+  removeChangeListener: function(callback) {
+    this.removeListener(CHANGE_EVENT, callback);
+  },
+
+  generateCID: function() {
+    return _current_cid++;
+  }
+
+});
+
+CargoBay.setMaxListeners(50);
+
+module.exports = CargoBay;
